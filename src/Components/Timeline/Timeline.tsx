729803546bdebaad5816fc86191a9b9f8ee4c9df
Erick Wesley Espinoza <33740245+WesleyEspinoza@users.noreply.github.com>
2024-04-09 11:53:27 -0500

    fix

--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -3,58 +3,44 @@ import "./Timeline.css"
 import TimeNode from "./TimeNode/TimeNode";
 
 type TimelineProps = {};
-type TimelineState = {};
+type TimelineState = { nodes: any[], startYear: number, endYear: Date };
 class Timeline extends React.Component<TimelineProps, TimelineState> {
     constructor(props: TimelineProps, state: TimelineState) {
         super(props);
-        this.state = state
+        this.state = { ...state, nodes: [], startYear: 1998, endYear: new Date() }
     }
+
+    getYears = () => {
+        const date2 = "10-21-1998";
+        let years = (this.state.endYear.getFullYear() - new Date(date2).getFullYear()) + 1;
+
+        let year = this.state.startYear
+        for (var i = 0; i < years; i++) {
+            const newNode = <TimeNode date={`${year}`} key={i} text="Radio Silence" />
+            const newNodes = this.state.nodes;
+            year++;
+            newNodes.push(newNode);
+            this.setState({ ...this.state, nodes: newNodes });
+
+        }
+    }
+    componentDidMount(): void {
+        this.getYears();
+        console.log(this.state.nodes)
+    }
+
 
     render() {
         return (
             <div className="TimelineContainer">
                 <div className="Timeline">
                     <div className="DottedLine" />
-                    <TimeNode date="October 21st 1998" text="Won the game of life" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-                    <TimeNode date="October 21st 1998" text="" />
-                    <TimeNode date="January 1st 2000" text="" />
-
-
+                    {this.state.nodes.map((node, index) => {
+                        return node
+                    })}
                 </div>
             </div>
         )
-
     }
 }
 
