a86efbb3d89cac399c8f6641d36f88933be4e00d
Erick Wesley Espinoza <33740245+WesleyEspinoza@users.noreply.github.com>
2024-04-09 21:37:29 -0500

    test

--- src/Components/Timeline/TimeNode/TimeNode.tsx
+++ src/Components/Timeline/TimeNode/TimeNode.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import "./TimeNode.css"
 
 type TimeNodeProps = { date: string, text: string };
-type TimeNodeState = { funFact: "Test Fact" };
+type TimeNodeState = { funFact: string };
 class TimeNode extends React.Component<TimeNodeProps, TimeNodeState> {
 
-    constructor(props: TimeNodeProps, state: TimeNodeState) {
+    constructor(props: TimeNodeProps, state: TimeNodeState = { funFact: "Get a fun Fact!" }) {
         super(props);
         this.state = state
+    }
+
+    getFact = (year: string) => {
+        const myHeaders = new Headers();
+        myHeaders.append("X-Api-Key", process.env.FACT_API_KEY!);
+        myHeaders.append("Content-Type", "application/json");
+
+        fetch(`https://api.api-ninjas.com/v1/historicalevents?text=${year}`, {
+            method: "GET",
+            headers: myHeaders,
+            credentials: "include", // Include cookies (if needed)
+        })
+            .then(response => response.json())
+            .then(data => {
+                const fact = data[Math.floor(Math.random() * data.length)]
+                this.setState({ ...this.state, funFact: fact });
+                console.log(this.state.funFact)
+            })
+            .catch(error => {
+                // Handle errors
+            });
     }
 
 
@@ -19,6 +40,7 @@ class TimeNode extends React.Component<TimeNodeProps, TimeNodeState> {
                     <div className="OuterDot" />
                     <h1 className="Date">{this.props.date}</h1>
                     <div className="Text"> {this.props.text} </div>
+                    <button onClick={() => this.getFact(this.props.date)}>{`${this.state.funFact}`}</button>
                     <div className="FunFact"> {this.state.funFact} </div>
                 </div>
             </div >
--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -10,17 +10,84 @@ class Timeline extends React.Component<TimelineProps, TimelineState> {
         this.state = { ...state, nodes: [], startYear: 1998, endYear: new Date() }
     }
 
+    addNode = (year: number, text: string, i: number) => {
+        const newNode = <TimeNode date={`${year}`} key={i} text={`${text}`} />
+        const newNodes = this.state.nodes;
+        newNodes.push(newNode);
+        this.setState({ ...this.state, nodes: newNodes });
+    }
+
     getYears = () => {
         const date2 = "10-21-1998";
         let years = (this.state.endYear.getFullYear() - new Date(date2).getFullYear()) + 1;
-
+        let text = "Radio Silence";
         let year = this.state.startYear
         for (var i = 0; i < years; i++) {
-            const newNode = <TimeNode date={`${year}`} key={i} text="Radio Silence" />
-            const newNodes = this.state.nodes;
+            switch (year) {
+                case 1998:
+                    text = "Won the Game of life!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2000:
+                    text = "Probably learned to walk, survived Y2K!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2007:
+                    text = "Migrated to the Unites States, learned to speak english.";
+                    this.addNode(year, text, i)
+                    break;
+                case 2008:
+                    text = "Earned student of the year!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2010:
+                    text = "Graduated Elementary school!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 20012:
+                    text = "Received a couple of Honor roll certificates.";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2016:
+                    text = "First AP Computer Science Class!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2017:
+                    text = "Graduated High school!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2018:
+                    text = "Accepted into accelerated bachelors in Computer Science program.";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2020:
+                    text = "Graduated College!";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2022:
+                    text = "First software Engineer job.";
+                    this.addNode(year, text, i)
+                    break;
+
+                case 2024:
+                    text = "Figuring it out.";
+                    this.addNode(year, text, i)
+                    break;
+
+                default:
+                    text = "Radio Silence";
+                    break;
+            }
             year++;
-            newNodes.push(newNode);
-            this.setState({ ...this.state, nodes: newNodes });
 
         }
     }
