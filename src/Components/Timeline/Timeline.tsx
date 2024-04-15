012a1e9a60269794c6181fd77570eef5ba65ca42
Erick Wesley Espinoza <33740245+WesleyEspinoza@users.noreply.github.com>
2024-04-15 08:45:11 -0500

    added time line back to mobile

--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -11,82 +11,76 @@ class Timeline extends React.Component<TimelineProps, TimelineState> {
     }
 
     addNode = (year: number, text: string, i: number) => {
-        const newNode = <TimeNode date={`${year}`} key={i} text={`${text}`} />
+        const newNode = <TimeNode date={year.toString()} key={i} text={text} />
         const newNodes = this.state.nodes;
         newNodes.push(newNode);
         this.setState({ ...this.state, nodes: newNodes });
     }
 
     getYears = () => {
-        const date2 = "10-21-1998";
-        let years = (this.state.endYear.getFullYear() - new Date(date2).getFullYear()) + 1;
+        let years = (this.state.endYear.getFullYear() - this.state.startYear) + 1;
+
         let text = "Radio Silence";
         let year = this.state.startYear
         for (var i = 0; i < years; i++) {
+
             switch (year) {
                 case 1998:
                     text = "Won the Game of life!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2000:
                     text = "Probably learned to walk, survived Y2K!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2007:
                     text = "Migrated to the Unites States, learned to speak english.";
-                    this.addNode(year, text, i)
                     break;
                 case 2008:
                     text = "Earned student of the year!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2010:
                     text = "Graduated Elementary school!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 20012:
                     text = "Received a couple of Honor roll certificates.";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2016:
                     text = "First AP Computer Science Class!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2017:
                     text = "Graduated High school!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2018:
                     text = "Accepted into accelerated bachelors in Computer Science program.";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2020:
                     text = "Graduated College!";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2022:
                     text = "First software Engineer job.";
-                    this.addNode(year, text, i)
                     break;
 
                 case 2024:
                     text = "Figuring it out.";
-                    this.addNode(year, text, i)
                     break;
 
                 default:
-                    text = "Radio Silence";
+                    text = "";
                     break;
             }
+
+            if (text !== "") {
+                this.addNode(year, text, i)
+            }
+
             year++;
         }
     }
